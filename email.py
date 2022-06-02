7607b982b034d48e600b09633e62e5d13c079673
Shiva Kumar <83355044+Shiva02Kumar@users.noreply.github.com>
2022-06-02 16:29:52 +0530

    Update email.py

--- email.py
+++ email.py
@@ -2,7 +2,7 @@ import smtplib
 
 
 def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com',587)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.login('youremail@gmail.com', 'your-password-here')
