b9cc852dc2320fca1cb01d71ddf853f914658258
Shiva Kumar <83355044+Shiva02Kumar@users.noreply.github.com>
2022-06-02 16:40:40 +0530

    Update email.py

--- email.py
+++ email.py
@@ -8,3 +8,9 @@ def sendEmail(to, content):
     server.login('youremail@gmail.com', 'your-password-here')
     server.sendmail('youremail@gmail.com', to, content)
     server.close()
+
+
+if __name__ == "__main__":
+    to = "sendmailto@gmail.com"
+    content = "content to send"
+    sendEmail(to, content)
